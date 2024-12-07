04972aa769d48945a022a772c852876dfce2f4f1
arnabw <arnabsahawrk@gmail.com>
2024-12-08 01:18:34 +0600

    Time and Space Complexity

---
+++ Basic Data Structures/Introduction to Data-Structure/1-Time and Space Complexity/Logarithmic_Complexity_O_logN.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;    // O(1)
+    cin >> n; // O(1)
+
+    for (int i = 1; i < n; i *= 2) // O(log2N) ignore constant so, O(logN)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl; // O(1)
+
+    for (int i = n; i >= 1; i /= 3) // O(log3N) ignore constant so, O(logN)
+    {
+        cout << i << " ";
+    }
+
+    int k = 2;                   // O(1)
+    for (int j = 1; j <= n; j++) // O(logkN) ignore constant so, O(logN)
+    {
+        cout << j << " ";
+        j = j * k;
+    }
+
+    return 0; // O(1)
+}
+
+/*
+n = 1000
+
+1    2    4    8    16    32    64    128    256    512
+2^0  2^1  2^2  2^3  2^4   2^5   ....................2^k
+
+log2^2^k = log2^1000
+klog2^2 = log2^1000
+k = log2^1000     [loga^a = 1]
+k = log2^N   [1000 = N]
+
+ignore the constant so, the operation will O(logN)
+
+If our code iteration will increment and decrement by * or / then the complexity will O(log N)
+*/
---
+++ Basic Data Structures/Introduction to Data-Structure/1-Time and Space Complexity/Time_Complexity.cpp
@@ -0,0 +1,24 @@
+/*
+Big O
+Linear Complexity --> O(N)
+Logarithmic Complexity --> O(logN)
+Sqrt Complexity --> O(sqrt(N))
+Quadratic Complexity --> O(N*N)
+Linearithmic Complexity --> O(NlogN)
+
+Time Complexity vary on OS, same can code execute takes different time in different OS. Time Complexity don't fix with times.
+
+O(N) - Order of N/Big O of N
+
+Asymptotic Notations:
+-->Omega notation (Ω) - Best Case
+-->Theta notation (θ) - Average Case
+-->Big O notation (O) - Worst Case
+
+Rules to calculate time complexity:
+1. Always take the worst case
+2. Ignore The Constant
+
+O(N-1), O(N/2), O(2N) --> ignore the constant O(N)
+ignore it because that doesn't make too much different if our code can execute O(N) it can execute O(N-1), O(N/2), O(2N).
+*/
