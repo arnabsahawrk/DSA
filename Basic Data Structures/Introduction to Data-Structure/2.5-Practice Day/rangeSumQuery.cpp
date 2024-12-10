7dc67c0dfe67deaea162bfda4e25cb62317a2eab
arnabw <arnabsahawrk@gmail.com>
2024-12-10 19:59:09 +0600

    Practice Day

--- Basic Data Structures/Introduction to Data-Structure/2.5-Practice Day/rangeSumQuery.cpp
+++ Basic Data Structures/Introduction to Data-Structure/2.5-Practice Day/rangeSumQuery.cpp
@@ -1,3 +1,5 @@
+// https : // codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Y
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Basic Data Structures/Introduction to Data-Structure/2.5-Practice Day/replacement.cpp
+++ Basic Data Structures/Introduction to Data-Structure/2.5-Practice Day/replacement.cpp
@@ -1,3 +1,5 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/C
+
 #include <bits/stdc++.h>
 using namespace std;
 
