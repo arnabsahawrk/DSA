ea502f87e7cf64c9dd78aba240dbb16a8de78710
arnabw <arnabsahawrk@gmail.com>
2024-12-10 21:09:06 +0600

    Practice Day

---
+++ Basic Data Structures/Introduction to Data-Structure/2.5-Practice Day/Divisor.cpp
@@ -0,0 +1,36 @@
+// https://lightoj.com/problem/dimik-divisor
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v;
+        for (int i = 1; i <= sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                v.push_back(i);
+
+                if (n / i != i)
+                    v.push_back(n / i);
+            }
+        }
+
+        sort(v.begin(), v.end());
+        cout << "Case " << i << ": ";
+        for (auto numb : v)
+            cout << numb << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
