2359ae133ce2379cbc73cdcdb1c6abba76e6dccc
arnabw <arnabsahawrk@gmail.com>
2024-12-17 21:49:52 +0600

    More Operation On Singly Linked List

---
+++ Basic Data Structures/Linked List/7-More Operation On Singly Linked List/delete_at_head.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = nullptr;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int data)
+{
+    Node *newNode = new Node(data);
+
+    if (head == nullptr)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void display(Node *node)
+{
+    while (node)
+    {
+        cout << node->value << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+void delete_at_head(Node *&head)
+{
+    if (head == nullptr)
+        return;
+
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+int main()
+{
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    int value;
+    while (cin >> value)
+        insert_at_tail(head, tail, value);
+
+    delete_at_head(head);
+
+    display(head);
+
+    return 0;
+}
