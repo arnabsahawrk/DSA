1e8ccf0e9269e06b9a18209557e3b5fdbb28e35c
arnabw <arnabsahawrk@gmail.com>
2025-01-06 00:36:39 +0600

    Queue Implementation and STL

---
+++ Basic Data Structures/Stack & Queue/14-Queue Implementation and STL/queue.cpp
@@ -0,0 +1,12 @@
+/*
+What is Queue?
+-> Queue is a Data Structure that follows FIFO techniques, which means data is inserted at the rear (end) and removed from the front.
+
+Queue is a (Abstract Data Stucture), (Higher Level Data Stucture).
+
+Not like (Array and Linked List is lower level Data Structure).
+
+FIFO -> Queue follow the First In, First Out (FIFO) principle.
+
+Queue Operation: enqueue(), dequeue(), front(), back(), size(), empty().
+*/
