d53a1fd681519a32288bf12b9fc807c810a37974
arnabw <arnabsahawrk@gmail.com>
2025-01-09 15:48:13 +0600

    Practice Day

---
+++ Basic Data Structures/Stack & Queue/15.5-Practice Day/Reverse First K elements of Queue.cpp
@@ -0,0 +1,28 @@
+// https://www.naukri.com/code360/problems/reverse-first-k-elements-of-queue_982771
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> reverseElements(queue<int> q, int k)
+{
+    stack<int> st;
+    queue<int> ans;
+
+    while (k--)
+    {
+        st.push(q.front());
+        q.pop();
+    }
+
+    while (!st.empty())
+    {
+        ans.push(st.top());
+        st.pop();
+    }
+
+    while (!q.empty())
+    {
+        ans.push(q.front());
+        q.pop();
+    }
+
+    return ans;
+}
